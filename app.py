577c4b1f6be3e259788de5c223deca993b18e9be
root <root@LAPTOP-DP17AMMN.localdomain>
2021-10-27 16:30:43 -0400

    results page 1.0

--- app.py
+++ app.py
@@ -15,58 +15,43 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def root():
-    if request.method == 'POST':
-        zip_code = request.form['zip']
-        range = request.form['range']
-        filter = request.form['breweryType']
+    return render_template("home.html")
 
-        # create user location that has latitude/longitude
-        user_location = Location(zip_code)
-  
-        """production version
-        # get the 20 closest breweries to coordinates
-        data = requests.get('https://api.openbrewerydb.org/breweries?by_dist='+user_location.get_y()+','+user_location.get_x()+'&per_page=20')
-        breweries = json.loads(data.text)  # convert object to json
-        """
-        # test version----------------------------------------
-        f = open('breweries.json')
-        breweries = json.load(f)
-        f.close()
-        #------------------------------------------------------
-
-        brewery_list = [] # list of brewery objects
-        for brewery in breweries:
-            brewer_location = Location(brewery['postal_code'], brewery['longitude'], brewery['latitude'])
-            new_brewery = Brewery(brewery['name'], brewery['brewery_type'], brewer_location)
-            brewery_list.append(new_brewery)
-            #print(brewery['name'])
-            
-        #print(response.text)
-
-        for b in brewery_list:
-            print(b.get_name() + str(b.get_location().get_x()) + ', ' + str(b.get_location().get_y()))
-            print(user_location.get_x() + ', ' + str(user_location.get_y()))
-        """
-        test = ''
-        for brewery in brewery_list:
-            #test += brewery[0]
-            test += brewery.get_name()
-            test += ', '
-            test += brewery.get_location().get_x()
-            test += '; '
-        """
-
-        
-
-        return 'dum'
-    
-    else:
-        return render_template("home.html")
-
-@app.route('/results')
+@app.route('/results', methods=['POST'])
 def get_results():
-    pass
-    
+
+    zip_code = request.form['zip']
+    range = request.form['range']
+    filter = request.form['breweryType']
+
+    # create user location that has latitude/longitude
+    user_location = Location(zip_code)
+  
+    """production version
+    # get the 20 closest breweries to coordinates
+    data = requests.get('https://api.openbrewerydb.org/breweries?by_dist='+user_location.get_y()+','+user_location.get_x()+'&per_page=20')
+    breweries = json.loads(data.text)  # convert object to json
+    """
+    # test version----------------------------------------
+    f = open('breweries.json')
+    breweries = json.load(f)
+    f.close()
+    #------------------------------------------------------
+
+    brewery_list = [] # list of brewery objects
+    for brewery in breweries:
+        brewer_location = Location(brewery['postal_code'], brewery['longitude'], brewery['latitude'])
+        new_brewery = Brewery(brewery['name'], brewery['brewery_type'], brewer_location)
+        brewery_list.append(new_brewery)
+        #print(brewery['name'])
+            
+    #print(response.text)
+
+    for b in brewery_list:
+        print(b.get_name() + str(b.get_location().get_x()) + ', ' + str(b.get_location().get_y()))
+        print(user_location.get_x() + ', ' + str(user_location.get_y()))
+
+    return render_template('results.html', results=brewery_list)
 
 @app.route('/test')
 def test():
